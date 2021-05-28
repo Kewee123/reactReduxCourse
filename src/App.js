273ebf8e02550565e32368d0bdc345d465a09632
Maung Naing <maungnaing1337@gmail.com>
2021-05-28 16:37:51 -0700

    add open source styles and start of naive approach to a component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 const App = () => {
-  return <div>Hi There !</div>; 
+  return (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar"/>
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog post</div>
+      </div>
+    </div>
+  </div>);
 }
 
 export default App;
