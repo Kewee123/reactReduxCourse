2eef4c597d186a7ba78105fa0a82f775360a1315
Maung Naing <maungnaing1337@gmail.com>
2021-06-04 11:10:17 -0700

    convert component to take props instead

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import CommentDetail  from './CommentDetail'
+import faker from 'faker';
 
 const App = () => {
   return (
   <div className="ui container comments">
-    {CommentDetail()}
-    {CommentDetail()}
-    {CommentDetail()}
+    <CommentDetail author="sam" date="Today at 1PM" text="hello" avatar={faker.image.avatar()}/>
+    <CommentDetail author="alex" date="Today at 12PM" text="hi" avatar={faker.image.avatar()}/>
+    <CommentDetail author="jane"date="Today at 3PM" text="welcome" avatar={faker.image.avatar()}/>
   </div>);
 }
 
--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+
+const CommentDetail = ({author, date, text, avatar}) => {
     return(
   <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={avatar}/>
         </a>
         <div className="content">
           <a href="/" className="author">
-            Sam
+            {author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{date}</span>
           </div>
-          <div className="text">Nice blog post</div>
+          <div className="text">{text}</div>
         </div>
       </div>
     );
